919473f7296126f957c81088d75c917fdd82680c
Vadim Koshechkin <rizus@yandex-team.ru>
2021-12-04 16:32:55 +0300

    create start script

--- dev-server.js
+++ dev-server.js
@@ -1,7 +1,7 @@
 import app from './app.js';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`)
+    console.log(`Example app listening at http://localhost:${PORT}`);
 });
